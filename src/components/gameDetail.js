12ededdb88143bdc54b55f04138fc2cce2a3ed75
Victor Mascarenhas Reis da Silva <victor.mascarenhas@al.infnet.edu.br>
2021-04-22 09:04:19 -0300

    Adding router

--- src/components/gameDetail.js
+++ src/components/gameDetail.js
@@ -6,35 +6,39 @@ import { useSelector } from "react-redux";
 
 const GameDetail = () => {
   //Data
-  const { screen, game } = useSelector((state) => state.detail);
+  const { screen, game, isLoading } = useSelector((state) => state.detail);
   return (
-    <CardShadow>
-      <Detail>
-        <Stats>
-          <div className="rating">
-            <h3>{game.name}</h3>
-            <p>Rating: {game.rating}</p>
-          </div>
-          <Info>
-            <h3>Plataforms</h3>
-            <Platforms>
-              {game.platforms.map((data) => (
-                <h3 key={data.platform.id}>{data.platform.name}</h3>
+    <>
+      {!isLoading && (
+        <CardShadow>
+          <Detail>
+            <Stats>
+              <div className="rating">
+                <h3>{game.name}</h3>
+                <p>Rating: {game.rating}</p>
+              </div>
+              <Info>
+                <h3>Plataforms</h3>
+                <Platforms>
+                  {game.platforms.map((data) => (
+                    <h3 key={data.platform.id}>{data.platform.name}</h3>
+                  ))}
+                </Platforms>
+              </Info>
+            </Stats>
+            <Media>
+              <img src={game.background_image} alt="bgimg" />
+            </Media>
+            <Description>{game.description_raw}</Description>
+            <div className="gallery">
+              {screen.results.map((screen) => (
+                <img src={screen.image} key={screen.id} alt="game" />
               ))}
-            </Platforms>
-          </Info>
-        </Stats>
-        <Media>
-          <img src={game.background_image} alt="bgimg" />
-        </Media>
-        <Description>{game.description_raw}</Description>
-        <div className="gallery">
-          {screen.results.map((screen) => (
-            <img src={screen.image} key={screen.id} alt="game" />
-          ))}
-        </div>
-      </Detail>
-    </CardShadow>
+            </div>
+          </Detail>
+        </CardShadow>
+      )}
+    </>
   );
 };
 
--- src/store/actions/detailAction.js
+++ src/store/actions/detailAction.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import { gameDetailsURL, gameScreenshotURL } from "../../api";
 
 export const loadDetail = (id) => async (dispatch) => {
+  dispatch({ type: "LOADING_DETAIL" });
   const detailData = await axios.get(gameDetailsURL(id));
   const screenShotData = await axios.get(gameScreenshotURL(id));
   dispatch({
